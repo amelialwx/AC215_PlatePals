87fb7917980042b79862212f2e3c4279e8cbbd0b
pwu97 <43555461+pwu97@users.noreply.github.com>
2023-09-24 15:50:40 -0400

    Update preprocess2.py

--- src/preprocessing/preprocess2.py
+++ src/preprocessing/preprocess2.py
@@ -8,39 +8,41 @@ import numpy as np
 
 # Constants
 IMG_SIZE = 128  # Resize images to 128x128 for faster processing
-BUCKET_NAME = os.environ.get('GCS_BUCKET_NAME', 'default-bucket-name')  # Retrieve from environment variable
-os.environ['GOOGLE_APPLICATION_CREDENTIALS']='./secrets/data-service-account.json'
+BUCKET_NAME = os.environ.get('GCS_BUCKET_NAME', 'default-bucket-name')  
+# Retrieve from environment variable
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './secrets/data-service-account.json'
 
 # URL of the nutritions dataset to download
 dataset_url = "https://raw.githubusercontent.com/prasertcbs/basic-dataset/master/nutrients.csv"
 
+# Google Cloud Storage object name
 gcs_object_name = "nutrients.csv"
 
 # Local file path to save the downloaded nutritions dataset
 local_file_path = "nutrients.csv"
 
-def preprocess_and_upload(image, label):
+def preprocess_and_upload(image, label_str):
     """Preprocess a single image and upload it to GCS."""
+    # Resize and cast the image
     img = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
     img = tf.cast(img, tf.uint8)
-    img_array = np.array(img)
-    pil_img = Image.fromarray(img_array)
 
-#    label_str = label.decode("utf-8")
-    label_str = label.encode().decode()
-#    blob_name = f"{label_str}/{tf.random.uniform(shape=[], minval=1, maxval=1e7, dtype=tf.int32)}.jpg"
+    # Encode tensor image to JPEG string
+    img_encoded = tf.image.encode_jpeg(img)
+
+    blob_name = f"{label_str}/{tf.random.uniform(shape=[], minval=1, maxval=int(1e7), dtype=tf.int32)}.jpg"
 
     # Initialize Google Cloud Storage client
     client = storage.Client()
     bucket = client.get_bucket(BUCKET_NAME)
 
-    blob = bucket.blob('dfsdadfsfads')
-    blob.upload_from_string(pil_img.tobytes(), content_type='image/jpeg')
+    blob = bucket.blob(blob_name)
+    blob.upload_from_string(img_encoded.numpy(), content_type='image/jpeg')
 
 def preprocess_data():
     """Load and preprocess the Food-101 dataset."""
     (ds_train, ds_test), ds_info = tfds.load(
-        'cifar10',
+        'mnist',
         split=['train[:1%]', 'test[:1%]'],
         shuffle_files=True,
         as_supervised=True,
@@ -55,7 +57,7 @@ def preprocess_data():
     # Process testing data
     for img, label in ds_test.take(100):  # Adjust the number as needed
         label_str = ds_info.features['label'].int2str(label)
-        preprocess_and_upload(img, label_str)        
+        preprocess_and_upload(img, label_str)     
     
 # Function to download the nutritions dataset from the URL
 def download_dataset(url, local_path):
@@ -73,10 +75,11 @@ def upload_to_gcs(bucket_name, source_file, destination_blob_name):
     blob.upload_from_filename(source_file)
 
 if __name__ == "__main__":
+    # Download and upload Food101 dataset
     preprocess_data()
-    
     # Download the nutritions dataset
     download_dataset(dataset_url, local_file_path)
     # Upload the downloaded nutritions dataset to GCS
     upload_to_gcs(BUCKET_NAME, local_file_path, gcs_object_name)
+    
     print(f"Preprocessed data uploaded to GCS bucket {BUCKET_NAME}")
