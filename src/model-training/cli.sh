42cddde14555f731922e68cc86da017ba50685f5
Amelia Li <weixili@g.harvard.edu>
2023-10-04 04:43:42 -0400

    add bucket name

--- src/model-training/cli.sh
+++ src/model-training/cli.sh
@@ -18,19 +18,19 @@ export CMDARGS="--model_name=mobilenetv2,--epochs=30,--batch_size=32,--wandb_key
 #export CMDARGS="--model_name=tfhub_mobilenetv2,--epochs=30,--batch_size=32,--wandb_key=$WANDB_KEY"
 #export CMDARGS="--model_name=tfhub_mobilenetv2,--train_base,--epochs=30,--batch_size=32,--wandb_key=$WANDB_KEY"
 
-gcloud ai custom-jobs create \
-  --region=$GCP_REGION \
-  --display-name=$DISPLAY_NAME \
-  --python-package-uris=$PYTHON_PACKAGE_URI \
-  --worker-pool-spec=machine-type=$MACHINE_TYPE,replica-count=$REPLICA_COUNT,accelerator-type=$ACCELERATOR_TYPE,accelerator-count=$ACCELERATOR_COUNT,executor-image-uri=$EXECUTOR_IMAGE_URI,python-module=$PYTHON_MODULE \
-  --args=$CMDARGS
-
-
-# Run training with No GPU
-# export EXECUTOR_IMAGE_URI="us-docker.pkg.dev/vertex-ai/training/tf-cpu.2-12.py310:latest"
 # gcloud ai custom-jobs create \
 #   --region=$GCP_REGION \
 #   --display-name=$DISPLAY_NAME \
 #   --python-package-uris=$PYTHON_PACKAGE_URI \
-#   --worker-pool-spec=machine-type=$MACHINE_TYPE,replica-count=$REPLICA_COUNT,executor-image-uri=$EXECUTOR_IMAGE_URI,python-module=$PYTHON_MODULE \
+#   --worker-pool-spec=machine-type=$MACHINE_TYPE,replica-count=$REPLICA_COUNT,accelerator-type=$ACCELERATOR_TYPE,accelerator-count=$ACCELERATOR_COUNT,executor-image-uri=$EXECUTOR_IMAGE_URI,python-module=$PYTHON_MODULE \
 #   --args=$CMDARGS
+
+
+# Run training with No GPU
+export EXECUTOR_IMAGE_URI="us-docker.pkg.dev/vertex-ai/training/tf-cpu.2-12.py310:latest"
+gcloud ai custom-jobs create \
+  --region=$GCP_REGION \
+  --display-name=$DISPLAY_NAME \
+  --python-package-uris=$PYTHON_PACKAGE_URI \
+  --worker-pool-spec=machine-type=$MACHINE_TYPE,replica-count=$REPLICA_COUNT,executor-image-uri=$EXECUTOR_IMAGE_URI,python-module=$PYTHON_MODULE \
+  --args=$CMDARGS
