7e77872101c4c17dc488f59117a9f407ef690344
pwu97 <43555461+pwu97@users.noreply.github.com>
2023-10-26 17:38:40 -0400

    Add some docstrings and typehints to preprocess.py

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -19,8 +19,13 @@ LOCAL_PATH, GCS_OBJECT_NAME = "nutrients.csv", "nutrients.csv"
 DATA_VERSION = "preprocessed_data" # CHANGE THIS
 
 
-def data_augmentation_layer():
-    """Create a Keras Sequential model for data augmentation."""
+def data_augmentation_layer() -> tf.keras.Sequential:
+    """
+    Create a Keras Sequential model for data augmentation.
+    
+    Returns:
+        A Keras Sequential model for data augmentation.
+    """
     return tf.keras.Sequential([
         preprocessing.RandomFlip('horizontal'),
         preprocessing.RandomRotation(0.2),
@@ -30,16 +35,42 @@ def data_augmentation_layer():
     ], name="data_augmentation")
 
 
-def preprocess_img(image, label, img_size=IMG_SIZE, augment_layer=None):
-    """Preprocess image and label."""
+def preprocess_img(image: tf.Tensor, label: tf.Tensor, 
+                   img_size: int = IMG_SIZE, 
+                   augment_layer: tf.keras.Sequential = None) -> tuple[tf.Tensor, tf.Tensor]:
+    """
+    Preprocess an image and its label.
+
+    Args:
+        image (tf.Tensor): The input image tensor.
+        label (tf.Tensor): The label tensor.
+        img_size (int, optional): The target image size. Defaults to IMG_SIZE.
+        augment_layer (tf.keras.Sequential, optional): An optional data augmentation layer. Defaults to None.
+
+    Returns:
+        tuple[tf.Tensor, tf.Tensor]: A tuple containing the preprocessed image and its label.
+    """
+
     if augment_layer:
         image = augment_layer(image)
     image = tf.image.resize(image, [img_size, img_size])
     return tf.cast(image, tf.float32), label
 
 
-def fetch_and_process_dataset(split_list, ds_info, augment_layer):
-    """Fetch and preprocess dataset given a list of splits (train, val, test etc.)."""
+def fetch_and_process_dataset(split_list: list[str], 
+                              ds_info: tfds.core.DatasetInfo, 
+                              augment_layer: tf.keras.Sequential) -> None:
+    """
+    Fetch and preprocess a dataset for a list of splits.
+
+    Args:
+        split_list (list[str]): A list of split names (e.g., 'train', 'val', 'test').
+        ds_info (tfds.core.DatasetInfo): Information about the dataset.
+        augment_layer (tf.keras.Sequential): A data augmentation layer.
+    
+    Returns:
+        None
+    """
     split_to_name = {
         'train': 'train',
         'validation[:40%]': 'val',
@@ -57,9 +88,21 @@ def fetch_and_process_dataset(split_list, ds_info, augment_layer):
     for custom_split, ds in datasets.items():
         create_zip_and_upload(ds, ds_info, custom_split)
 
+        
+def create_zip_and_upload(ds: tf.data.Dataset, 
+                          ds_info: tfds.core.DatasetInfo, 
+                          split: str) -> None:
+    """
+    Create a ZIP of preprocessed images and upload it to Google Cloud Storage (GCS).
 
-# Function to zip and upload preprocessed images to GCS
-def create_zip_and_upload(ds, ds_info, split):
+    Args:
+        ds (tf.data.Dataset): The dataset containing preprocessed images and labels.
+        ds_info (tfds.core.DatasetInfo): Information about the dataset.
+        split (str): The name of the dataset split (e.g., 'train', 'val', 'test').
+
+    Returns:
+        None
+    """
     zip_buffer = io.BytesIO()
     # Keep track of generated file names to avoid duplicates
     seen_files = set()
@@ -88,9 +131,18 @@ def create_zip_and_upload(ds, ds_info, split):
     blob.chunk_size = 5 * 1024 * 1024
     blob.upload_from_file(zip_buffer, content_type='application/zip')
 
+    
+def download_nutrients_dataset(url: str, local_path: str) -> None:
+    """
+    Download a nutrients dataset from a URL and save it locally.
 
-# Function to download the nutrients dataset
-def download_nutrients_dataset(url, local_path):
+    Args:
+        url (str): The URL of the nutrients dataset.
+        local_path (str): The local path where the dataset will be saved.
+        
+    Returns: 
+        None
+    """
     if os.path.exists(local_path):
         os.remove(local_path)
 
@@ -100,9 +152,21 @@ def download_nutrients_dataset(url, local_path):
     with open(local_path, "wb") as f:
         f.write(response.content)
 
+        
+def upload_nutrients_to_gcs(bucket_name: str, 
+                            source_file: str, 
+                            destination_blob_name: str) -> None:
+    """
+    Upload a nutrients dataset from a local file to Google Cloud Storage (GCS).
 
-# Function to upload nutrients dataset to GCS
-def upload_nutrients_to_gcs(bucket_name, source_file, destination_blob_name):
+    Args:
+        bucket_name (str): The name of the GCS bucket.
+        source_file (str): The local file to be uploaded.
+        destination_blob_name (str): The name of the destination blob in GCS.
+
+    Returns: 
+        None
+    """
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(f"{DATA_VERSION}/{destination_blob_name}")  
