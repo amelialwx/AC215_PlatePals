ff84254c932aafcaf8b7227828efbcf5bcec9637
Amelia Li <weixili@g.harvard.edu>
2023-11-09 16:46:41 -0500

    remove model folder

--- src/model/effnetv2b0/package-trainer.sh
+++
@@ -1,4 +0,0 @@
-rm -f effnetv2b0-trainer.tar effnetv2b0-trainer.tar.gz
-tar cvf effnetv2b0-trainer.tar package
-gzip effnetv2b0-trainer.tar
-gsutil cp effnetv2b0-trainer.tar.gz $GCS_BUCKET_URI/platepals-effnetv2b0-trainer.tar.gz
