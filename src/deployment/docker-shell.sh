264bfcb1e8a970eb68b0c8adcadadd8192a505d8
Amelia Li <weixili@g.harvard.edu>
2023-12-04 16:16:48 -0500

    C:/Program Files/Git/run-deploy-app

--- src/deployment/docker-shell.sh
+++ src/deployment/docker-shell.sh
@@ -10,6 +10,10 @@ export SECRETS_DIR=$(pwd)/../../../secrets/
 export GCP_PROJECT="platepals-405005" # Change to your GCP Project
 export GCP_ZONE="us-central1-a"
 export GOOGLE_APPLICATION_CREDENTIALS=/../secrets/deployment.json
+export GCS_BUCKET_NAME="platepals-trainer"
+export GCS_SERVICE_ACCOUNT="data-service-account@platepals-405005.iam.gserviceaccount.com"
+export GCP_REGION="us-central1"
+export GCS_PACKAGE_URI="gs://platepals-trainer"
 
 # Build the image based on the Dockerfile
 docker build -t $IMAGE_NAME -f Dockerfile .
@@ -23,9 +27,14 @@ winpty docker run --rm --name $IMAGE_NAME -ti \
 --mount type=bind,source="$HOME/.ssh",target=/home/app/.ssh \
 --mount type=bind,source="$BASE_DIR/../api-service",target=/api-service \
 --mount type=bind,source="$BASE_DIR/../frontend-react",target=/frontend-react \
+--mount type=bind,source="$BASE_DIR/../preprocessing",target=/preprocessing \
 -e GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS \
 -e USE_GKE_GCLOUD_AUTH_PLUGIN=True \
 -e GCP_PROJECT=$GCP_PROJECT \
 -e GCP_ZONE=$GCP_ZONE \
+-e GCS_BUCKET_NAME=$GCS_BUCKET_NAME \
+-e GCS_SERVICE_ACCOUNT=$GCS_SERVICE_ACCOUNT \
+-e GCP_REGION=$GCP_REGION \
+-e GCS_PACKAGE_URI=$GCS_PACKAGE_URI \
 $IMAGE_NAME
 
